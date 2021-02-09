6fdd5a91351b2ea1294a70de8279ad8283e0160a
Christian Parker <christianparker512@gmail.com>
2021-02-09 14:48:42 -0600

    math increment +1

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,9 +1,8 @@
 package com.codeup.springblog.controllers;
 
+import org.apache.catalina.connector.Request;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -17,5 +16,10 @@ public class HelloController {
     @ResponseBody
     public String sayHello(@PathVariable String name){
         return "Hello " + name + "!";
+    }
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number){
+        return number + " plus one is " + (number + 1) + "!";
     }
 }
