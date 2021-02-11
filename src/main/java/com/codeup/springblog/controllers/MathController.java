691099d278c51d1d69647d6e588d1802b106710b
Christian Parker <christianparker512@gmail.com>
2021-02-11 11:09:24 -0600

    post controller and roll the dice in the view exercise

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -16,7 +16,7 @@ public class MathController {
     @GetMapping("/subtract/{number1}/from/{number2}")
     @ResponseBody
     public String subtract(@PathVariable int number1, @PathVariable int number2) {
-        return number2 + " - " + number1 + " = " +(number2 - number1);
+        return number2 + " - " + number1 + " = " + (number2 - number1);
     }
     @GetMapping("/subtract/{number1}/from/{number2}/and/{number3}")
     @ResponseBody
@@ -26,7 +26,7 @@ public class MathController {
     @GetMapping("/multiply/{num1}/and/{num2}")
     @ResponseBody
     public int multiply(@PathVariable int number1, @PathVariable int number2) {
-        return number2 * number1;
+        return number1 * number2;
     }
 
     @GetMapping("/divide/{num1}/by/{num2}")
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,36 +1,54 @@
 package com.codeup.springblog.controllers;
 
+
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+
+    @GetMapping("/posts")
+    public String postsIndex(Model model){
+        Post post1 = new Post("When one door closes, another opens...", "", 1);
+        Post post2 = new Post("So it begins...Again", "Life happens...I know. I started Codeup the first time and made it through the first month. Casey got sick and then it was too much. Sometimes you need to take a step back and restart. It turned out for the best.  ", 2);
+        Post post3 = new Post("Sea legs", "I remember when I ran a marathon. I walked for a minute and then ran for a minute. It was tough getting my legs back. I'm feeling like this is the way that this coding is for me. ", 3);
+
+    List<Post> postList = new ArrayList<>();
+        postList.add(post1);
+        postList.add(post2);
+        postList.add(post3);
+
+        model.addAttribute("title", "All Posts");
+        model.addAttribute("posts", postList);
+
+        return "posts/index";
+}
+
+    @GetMapping("/posts/{id}")
+    public String postView(Model model, @PathVariable String id) {
+//        get single post by id later
+        Post post = new Post("First Post", "This is my first post", 1);
+        model.addAttribute("title", "Single Posts");
+        model.addAttribute("post", post);
+        return "posts/show";
+    }
+    @GetMapping("/posts/create")
     @ResponseBody
-    public String all(){
-        return "posts index page";
+    public String postForm(){
+        return "Create a post here!";
     }
 
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String post(long id){
-        return "view an individual post";
-    }
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
-    public String createForm(){
-        return "view the form for creating a post";
-    }
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @PostMapping("/posts/create")
     @ResponseBody
     public String createPost(){
-        return "create a new post";
+        return "Creating a new post...";
     }
-
 }
