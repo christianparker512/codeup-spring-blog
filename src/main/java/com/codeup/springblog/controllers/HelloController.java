4d0c5ac1ff0027e43f19e75125c14d4ef20c8acd
Christian Parker <christianparker512@gmail.com>
2021-02-15 10:58:18 -0600

    Join template and hello controller revision

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -2,28 +2,41 @@ package com.codeup.springblog.controllers;
 
 import org.apache.catalina.connector.Request;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
-//    This will just say hello from spring since it has no variable
+    //    This will just say hello from spring since it has no variable
     @GetMapping("/hello")
     @ResponseBody
-        public String hello(){
-            return "Hello from Spring!";
+    public String hello() {
+        return "Hello from Spring!";
     }
+
     //The code below will result in whomever it saying hello to whomever the person says in the url.
     @GetMapping("/hello/{name}")
     @ResponseBody
-    public String sayHello(@PathVariable String name){
+    public String sayHello(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 
     //The below will result in an incremental output.
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
-    public String addOne(@PathVariable int number){
+    public String addOne(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
     }
 }
