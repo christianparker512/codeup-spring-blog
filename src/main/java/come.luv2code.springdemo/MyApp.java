0f257e720199a5563c8036d484323abed41d9682
Christian Parker <christianparker512@gmail.com>
2021-02-15 15:25:35 -0600

    Change coach to track coach

--- src/main/java/come.luv2code.springdemo/MyApp.java
+++ src/main/java/come.luv2code.springdemo/MyApp.java
@@ -4,7 +4,8 @@ public class MyApp {
     public static void main(String[] args) {
         ///TODO Auto generated method studb
         //create the object
-       Coach theCoach = new BaseballCoach();
+       Coach theCoach = new TrackCoach();
+
         //Use the object
         System.out.println(theCoach.getDailyWorkout());
     }
