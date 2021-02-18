bba767b2d2aee6a82378ae238dad25f2d473abb4
Christian Parker <christianparker512@gmail.com>
2021-02-18 11:10:58 -0600

    adding missing thymeleaf.org link to the show

--- src/main/java/com/codeup/springblog/models/Ad.java
+++ src/main/java/com/codeup/springblog/models/Ad.java
@@ -23,38 +23,38 @@ public class Ad {
 
     }
 //
-//    public long getId() {
-//        return id;
-//    }
+    public long getId() {
+        return id;
+    }
 //
-//    public void setId(long id) {
-//        this.id = id;
-//    }
+    public void setId(long id) {
+        this.id = id;
+    }
 //
-//    public String getTitle() {
-//        return title;
-//    }
+    public String getTitle() {
+        return title;
+    }
 //
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 //
-//    public String getDescription() {
-//        return description;
-//    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 //
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public Ad(long id, String title, String description){
-//        this.id = id;
-//        this.title = title;
-//        this.description = description;
-//    }
-//    public Ad(String title, String description){
-//
-//        this.title = title;
-//        this.description = description;
-//    }
+    public Ad(long id, String title, String description){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+    public Ad(String title, String description){
+
+        this.title = title;
+        this.description = description;
+    }
 }
