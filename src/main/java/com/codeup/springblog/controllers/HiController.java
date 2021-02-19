3ca7a44ceb2424ae63c22d94e26ff7abf2c309cd
Christian Parker <christianparker512@gmail.com>
2021-02-19 17:07:19 -0600

    Request mapping for greeting, then changed to basic first and then proper in a method overloading style

--- src/main/java/com/codeup/springblog/controllers/HiController.java
+++ src/main/java/com/codeup/springblog/controllers/HiController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greeting")
 public class HiController {
-    @RequestMapping(value ="/hi", method = RequestMethod.GET)
+    @RequestMapping(value ="/basic", method = RequestMethod.GET)
     public String sayHello(){
         return "<h1>hello you!</h1>";
+    }
+    @RequestMapping(value ="/proper", method = RequestMethod.GET)
+    public String sayProperHello(){
+        return "<h1>Good day! May I ask how you're doing today?</h1>";
     }
 }
