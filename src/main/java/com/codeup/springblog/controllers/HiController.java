281189071ea975e143e0d7e1551eb69878611f29
Christian Parker <christianparker512@gmail.com>
2021-02-19 17:22:38 -0600

    /user_entry form with form right from w3 schools

--- src/main/java/com/codeup/springblog/controllers/HiController.java
+++ src/main/java/com/codeup/springblog/controllers/HiController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class HiController {
-    @RequestMapping(value ="/basic", method = RequestMethod.GET)
+//    Don't need the get request or the value because it's default
+//    @RequestMapping(value ="/basic", method = RequestMethod.GET)
+    @RequestMapping("/basic")
     public String sayHello(){
         return "<h1>hello you!</h1>";
     }
@@ -15,4 +17,15 @@ public class HiController {
     public String sayProperHello(){
         return "<h1>Good day! May I ask how you're doing today?</h1>";
     }
+    @RequestMapping("/user_entry")
+    public String userForm() {
+        return "<form action=\"/user_greeting\" method=\"Post\">\n" +
+                "  <label for=\"fname\">First name:</label><br>\n" +
+                "  <input type=\"text\" id=\"fname\" name=\"fname\" value=\"write your name here\"><br>\n" +
+                "  <label for=\"lname\">Last name:</label><br>\n" +
+                "  <input type=\"text\" id=\"lname\" name=\"lname\" value=\"write your last name here\"><br><br>\n" +
+                "  <input type=\"submit\" value=\"Submit\">\n" +
+                "</form> ";
+    }
+
 }
