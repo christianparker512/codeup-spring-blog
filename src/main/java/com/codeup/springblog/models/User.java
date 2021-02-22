3e4954beccdb50318c130e42937a70d50dc6ce87
Christian Parker <christianparker512@gmail.com>
2021-02-22 13:49:25 -0600

    user class generated in the model declaring id, username, email and password

--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -1,4 +1,96 @@
 package com.codeup.springblog.models;
 
 public class User {
-}
+//    private String fullName;
+//    private int age;
+//    private boolean employed;
+//    private String gender;
+
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //
+//    public String getFullName() {
+//        return fullName;
+//    }
+//
+//    public void setFullName(String fullName) {
+//        this.fullName = fullName;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public boolean isEmployed() {
+//        return employed;
+//    }
+//
+//    public void setEmployed(boolean employed) {
+//        this.employed = employed;
+//    }
+//
+//    public String getGender() {
+//        return gender;
+//    }
+//
+//    public void setGender(String gender) {
+//        this.gender = gender;
+//    }
+//
+//    public User(String fullName, int age, boolean employed, String gender) {
+//        this.fullName = fullName;
+//        this.age = age;
+//        this.employed = employed;
+//        this.gender = gender;
+
+    }
+
+
