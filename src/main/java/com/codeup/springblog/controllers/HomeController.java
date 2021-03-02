a13a0bbac7f75d2d63fb35d21cd759e1d8ba2998
Christian Parker <christianparker512@gmail.com>
2021-03-02 16:23:53 -0600

    connected to database online at christianparker512.com

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "Welcome Home";
+        return "This is the landing page";
     }
 
 
